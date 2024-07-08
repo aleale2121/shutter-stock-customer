7fd08d0a2b8a608541ae585d8eb8c5b8c2d254ab
alefew <alefewyimer4@gmail.com>
2024-07-08 16:57:37 +0300

    fix color of page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -5,23 +5,23 @@ export default function PageNotFound() {
   return (
     <>
       <NavBar />
-      <section className="relative z-10 bg-primary py-[120px]">
+      <section className="relative z-10 py-[120px]">
         <div className="container mx-auto">
           <div className="-mx-4 flex">
             <div className="w-full px-4">
               <div className="mx-auto max-w-[400px] text-center">
-                <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
+                <h2 className="mb-2 text-[50px] font-bold leading-none text-veryDarkViolet sm:text-[80px] md:text-[100px]">
                   404
                 </h2>
-                <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
-                  Oops! That page can’t be found
+                <h4 className="mb-3 text-[22px] font-semibold leading-tight text-veryDarkViolet">
+                  Oops! That page cant be found
                 </h4>
-                <p className="mb-8 text-lg text-white">
+                <p className="mb-8 text-lg text-veryDarkViolet">
                   The page you are looking for it maybe deleted
                 </p>
                 <a
-                  href="javascript:void(0)"
-                  className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-primary"
+                  href="#"
+                  className="inline-block rounded-lg border border-veryDarkViolet px-8 py-3 text-center text-base font-semibold text-veryDarkViolet transition hover:bg-veryDarkViolet hover:text-white"
                 >
                   Go To Home
                 </a>
