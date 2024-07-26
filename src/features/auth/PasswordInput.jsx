1f70acc3ca7111f7ffe67ab40f43f3bd025d9b96
alefew <alefewyimer4@gmail.com>
2024-07-26 23:28:23 +0300

    Password Input

---
+++ src/features/auth/PasswordInput.jsx
@@ -0,0 +1,11 @@
+function PasswordInput() {
+  return (
+    <input
+      type="password"
+      className="w-full px-3 py-2 mt-6 border border-gray-300 rounded-md placeholder:font-sans placeholder:font-light"
+      placeholder="Enter your password"
+    />
+  );
+}
+
+export default PasswordInput;
